471760c3dc1c788189a3f5857dd541d9d5a5446a
alex <alsepkin@gmail.com>
2020-07-14 14:12:10 +0300

    HW 4

--- src/ru/javawebinar/basejava/MainArray.java
+++ src/ru/javawebinar/basejava/MainArray.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.basejava;
 
-
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.ArrayStorage;
 import ru.javawebinar.basejava.storage.Storage;
@@ -34,14 +33,12 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
                 case "update":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;
