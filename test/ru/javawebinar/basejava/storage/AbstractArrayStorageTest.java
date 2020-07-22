5dc75f1c5c6b271a44540e324654e526a7b8e756
alex <alsepkin@gmail.com>
2020-07-22 12:58:38 +0300

    HW 5 part 1

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -25,7 +25,7 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_4 = "uuid4";
     private static final Resume RESUME_4 = new Resume(UUID_4);
 
-    protected AbstractArrayStorageTest(Storage storage) {
+    public AbstractArrayStorageTest(Storage storage) {
        this.storage = storage;
     }
 
